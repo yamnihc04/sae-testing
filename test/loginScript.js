b10e478da39fe3c99a521ee6a53148a0fd34fc9c
Chinmay-Mahagaonkar <62831524+yamnihc04@users.noreply.github.com>
2020-08-11 15:16:27 +0530

    Add files via upload

---
+++ test/loginScript.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBfopBTDB6QTcQc_iIP5tw3CrhEvZwr3jw",
+    authDomain: "test-7ffea.firebaseapp.com",
+    databaseURL: "https://test-7ffea.firebaseio.com",
+    projectId: "test-7ffea",
+    storageBucket: "test-7ffea.appspot.com",
+    messagingSenderId: "116441375499",
+    appId: "1:116441375499:web:6d49a3d0d2ff9c1543f80e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+const txtEmail = document.getElementById("txtEmail");
+const txtPassword = document.getElementById("txtPassword");
+const btnLogin = document.getElementById("btnLogin");
+const btnSignup = document.getElementById("btnSignup");
+
+//Login Event
+ btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+
+    const auth = firebase.auth();
+
+    
+    auth.signInWithEmailAndPassword(email, password).then(user =>{
+        alert("Login Successful :)");
+    }).catch(err => {
+        alert(err.message);
+    });
+    
+});
